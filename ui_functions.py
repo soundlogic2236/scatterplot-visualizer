924d85e5bd7d76d946302f50de418612f21a207c
taygetea <taygetea@gmail.com>
2014-12-21 17:27:14 -0500

    Create ui_functions.py

---
+++ ui_functions.py
@@ -0,0 +1,23 @@
+from visual import *
+from visual.controls import *
+import wx
+import scatterplot
+import random
+
+def open_file(evt):
+	open_dialog=wx.FileDialog(None,"Open Dataset","","","CSV files (*.csv)|*.csv|Any files (*)|*",style=wx.FD_OPEN)
+	if ui.debug:
+		scatterplot.import_datafile('points.csv')
+	if open_dialog.ShowModal()==wx.ID_OK:
+		filename=open_dialog.GetPath()
+		scatterplot.import_datafile(filename)
+	scatterplot.render()
+
+def toggle_axes(evt):
+    scatterplot.axis_enabled=not scatterplot.axis_enabled
+    scatterplot.render()
+
+def toggle_grid(evt):
+	scatterplot.grid_faces_enabled = (bool(random.getrandbits(1)),bool(random.getrandbits(1)),bool(random.getrandbits(1)),bool(random.getrandbits(1)),bool(random.getrandbits(1)),bool(random.getrandbits(1)))
+	scatterplot.render()
+
